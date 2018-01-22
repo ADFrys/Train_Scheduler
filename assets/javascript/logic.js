e2e7a8223d7d29eb72e48b2ea95be66f36c1c06f
AngelaF <ADFrys@users.noreply.github.com>
2018-01-21 22:36:41 -0600

    Create UI.

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+
+var config = {
+    apiKey: "AIzaSyBKKii4pzj3RzZ614BM3Ei1LRcoVTXFGDQ",
+    authDomain: "trainschedule-87f2d.firebaseapp.com",
+    databaseURL: "https://trainschedule-87f2d.firebaseio.com",
+    projectId: "trainschedule-87f2d",
+    storageBucket: "",
+    messagingSenderId: "60004819055"
+  };
+  firebase.initializeApp(config);
